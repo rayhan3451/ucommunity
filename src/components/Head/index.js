e12709926c8af5fbb91a69ab20f6e8678ec7a08b
rayhan <md.rayhan3451@gmail.com>
2021-07-03 16:36:47 +0600

    first commit

---
+++ src/components/Head/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Navbar from '../Navbar'
+
+function index() {
+ return (
+  <div>
+   <Navbar/>
+
+   {/* Member_Slide */}
+  </div>
+ )
+}
+
+export default index
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./style.css"
+
+import {MenuAlt2Icon} from "@heroicons/react/outline"
+
+function index() {
+ return (
+  <div>
+   I am Navbar
+  </div>
+ )
+}
+
+export default index
